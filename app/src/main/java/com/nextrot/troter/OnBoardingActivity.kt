e25ccf6d0460a9fb1b2f7e320c14ca58a0e6ed30
Mincheol Kim <mcauto@naver.com>
2020-02-19 20:22:04 +0900

    feat(onboarding): change subtitle, lottie

--- app/src/main/java/com/nextrot/troter/OnBoardingActivity.kt
+++ app/src/main/java/com/nextrot/troter/OnBoardingActivity.kt
@@ -19,19 +19,19 @@ class OnBoardingActivity : AppCompatActivity(){
     private val boardings: List<OnBoarding> = listOf(
         OnBoarding(
             "트로트 듣기",
-            "듣고 싶은 노래를 선택하면 한 번에\n재생할 수 있어유 <b>쉽쥬?</b>",
+            "듣고 싶은 노래를 선택하면 한 번에<br>재생할 수 있어유 <b>쉽쥬?</b>",
             "onboarding_play.json",
             "다음",
             R.drawable.ic_paging_1),
         OnBoarding(
             "가사 보기",
-            "듣고 있는 노래의 가사를 보면서\n함께 따라 불러보셔유 <b>기가막히쥬?</b>",
+            "듣고 있는 노래의 가사를 보면서<br>함께 따라 불러보셔유 <b>기가막히쥬?</b>",
             "onboarding_lylics.json",
             "다음",
             R.drawable.ic_paging_2),
         OnBoarding(
             "확대 하기",
-            "가사가 잘 안보이면 오른쪽 위\n버튼을 눌러 확대해 보셔유! <b>지화자~!</b>",
+            "가사가 잘 안보이면 확대하<br>버튼을 눌러 크 보셔유! <b>지화자~!</b>",
             "onboarding_repeat.json",
             "트로트 들으러 가기",
             R.drawable.ic_paging_3)
@@ -45,17 +45,18 @@ class OnBoardingActivity : AppCompatActivity(){
         onClickNext()
     }
 
-    @SuppressLint("CommitPrefEdits")
     fun onClickNext(){
         if(!it.hasNext()){
             skipOnboarding()
         } else {
             val item = it.next()
+            onBoardingActivityBinding.lottieAnimationView.apply {
+                setAnimation(item.lottie)
+                repeatCount = 0
+                playAnimation()
+            }
             onBoardingActivityBinding.title.text=item.title
             onBoardingActivityBinding.subtitle.text= HtmlCompat.fromHtml(item.subtitle, HtmlCompat.FROM_HTML_MODE_LEGACY)
-            onBoardingActivityBinding.lottieAnimationView.setAnimation(item.lottie)
-            onBoardingActivityBinding.lottieAnimationView.repeatCount = 0
-            onBoardingActivityBinding.lottieAnimationView.playAnimation()
             onBoardingActivityBinding.nextButton.text=item.next
             onBoardingActivityBinding.indicator.setImageResource(item.indicator)
         }
